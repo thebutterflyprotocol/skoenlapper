4c90705892a91411d762e908e170a5f673c11df5
Tristan B. Kildaire <deavmi@disroot.org>
2020-06-18 18:07:25 +0200

    Removed testin print.

--- source/app.d
+++ source/app.d
@@ -4,8 +4,6 @@ import std.socket;
 
 void main()
 {
-	writeln("Edit source/app.d to start your project.");
-
 	/* TEST: Connect to server */
 	ButterflyClient d = new ButterflyClient(parseAddress("0.0.0.0", 6969));
 
