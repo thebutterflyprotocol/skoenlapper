839dcbbf237bcc5c2d2102d8676f1a1e9ca1da38
Tristan B. Kildaire <deavmi@disroot.org>
2020-06-18 17:55:59 +0200

    Upgraded to new libutterfly and accordingly added account registration test.

--- source/app.d
+++ source/app.d
@@ -9,13 +9,16 @@ void main()
 	/* TEST: Connect to server */
 	ButterflyClient d = new ButterflyClient(parseAddress("0.0.0.0", 6969));
 
+	/* TEST: Register with server */
+	d.register("deavmi", "password");
+
 	/* TEST: Autheticate with server */
 	d.authenticate("deavmi", "password");
 
 
 	while(true)
 	{
-		
+
 	}
 	
 }
