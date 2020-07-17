15d9e246a6261971f80ce00b7e0ee99a0d3255e0
Tristan B. Kildaire <deavmi@disroot.org>
2020-07-17 17:22:56 +0200

    Refactored registration test.

--- source/app.d
+++ source/app.d
@@ -2,66 +2,81 @@ import std.stdio;
 import libutterfly.client;
 import std.socket;
 import std.json;
+import std.conv : to;
 
-void main()
+
+void testRegistration()
 {
 	/* Create a new butterfly client */
-	ButterflyClient d = new ButterflyClient(parseAddress("0.0.0.0", 6969));
+	ButterflyClient d = new ButterflyClient(parseAddress("10.0.0.9", 2222));
 
 	/* TEST: Register with server */
 	d.register("kwaranpyn", "password");
+}
 
-	/* TEST: Connect to server */
-	d = new ButterflyClient(parseAddress("0.0.0.0", 6969));
+void main()
+{
+	/* Test account registration */
+	testRegistration();
 
-	/* TEST: Register with server */
-	d.register("deavmi", "password");
 
-	/* TEST: Authenticate with server */
-	d.authenticate("deavmi", "password");
+	// /* Create a new butterfly client */
+	// ButterflyClient d = new ButterflyClient(parseAddress("10.0.0.9", 2223));
 
-	/* TEST: Listing of mail messages */
-	string[] mailIDs = d.listMail("Drafts");
-	writeln(mailIDs);
+	// /* TEST: Register with server */
+	// d.register("kwaranpyn", "password");
 
-	/* TEST: Storing of mail message */
-	JSONValue mailMessage;
-	JSONValue[] recipients;
-	recipients ~= JSONValue("deavmi@poes");
-	recipients ~= JSONValue("kwaranpyn@10.1.0.7");
-	mailMessage["recipients"] = recipients;
-	mailMessage["subject"] = "Hello daar";
-	mailMessage["body"] = "Hi\noOOOOOOOOOOOo\n\nI'm never gonna find a wife";
-	d.storeMail("Drafts", mailMessage);
+	// /* TEST: Connect to server */
+	// d = new ButterflyClient(parseAddress("10.0.0.9", 2222));
 
-	/* TEST: Sending of mail */
-	d.sendMail(mailMessage);
-	mailIDs = d.listMail("Inbox");
-	JSONValue mail = d.fetchMail("Inbox", mailIDs[0]);
-	writeln("Sent message: " ~ mail.toPrettyString);
+	// /* TEST: Register with server */
+	// d.register("deavmi", "password");
 
-	/* TEST: Listing of mail */
-	mailIDs = d.listMail("Drafts");
-	writeln(mailIDs);
+	// /* TEST: Authenticate with server */
+	// d.authenticate("deavmi", "password");
 
-	/* TEST: Fetch mail */
-	mail = d.fetchMail("Drafts", mailIDs[0]);
-	writeln(mail.toPrettyString);
+	// /* TEST: Listing of mail messages */
+	// string[] mailIDs = d.listMail("Drafts");
+	// writeln("Drafts: " ~to!(string)(mailIDs));
 
-	/* TEST: Listing of folders */
-	string[] folderNames = d.listFolder("Drafts");
-	writeln(folderNames);
+	// /* TEST: Storing of mail message */
+	// JSONValue mailMessage;
+	// JSONValue[] recipients;
+	// recipients ~= JSONValue("deavmi@10.0.0.9:2222");
+	// recipients ~= JSONValue("kwaranpyn@10.0.0.9:2223");
+	// mailMessage["recipients"] = recipients;
+	// mailMessage["subject"] = "Hello daar";
+	// mailMessage["body"] = "Hi\noOOOOOOOOOOOo\n\nI'm never gonna find a wife";
+	// d.storeMail("Drafts", mailMessage);
 
-	/* TEST: Adding a folder */
-	d.createFolder("Drafts/testFolder");
+	// /* TEST: Sending of mail */
+	// d.sendMail(mailMessage);
+	// mailIDs = d.listMail("Inbox");
+	// JSONValue mail = d.fetchMail("Inbox", mailIDs[0]);
+	// writeln("Sent message: " ~ mail.toPrettyString);
 
-	/* TEST: Listing of folders */
-	folderNames = d.listFolder("Drafts");
-	writeln(folderNames);
+	// /* TEST: Listing of mail */
+	// mailIDs = d.listMail("Drafts");
+	// writeln(mailIDs);
 
-	while(true)
-	{
+	// /* TEST: Fetch mail */
+	// mail = d.fetchMail("Drafts", mailIDs[0]);
+	// writeln(mail.toPrettyString);
 
-	}
+	// /* TEST: Listing of folders */
+	// string[] folderNames = d.listFolder("Drafts");
+	// writeln(folderNames);
+
+	// /* TEST: Adding a folder */
+	// d.createFolder("Drafts/testFolder");
+
+	// /* TEST: Listing of folders */
+	// folderNames = d.listFolder("Drafts");
+	// writeln(folderNames);
+
+	// while(true)
+	// {
+
+	// }
 	
 }
