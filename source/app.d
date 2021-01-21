443ecbd114450c4c6a85e182b13d2900f1d61f98
Tristan B. Kildaire <deavmi@disroot.org>
2021-01-21 12:50:38 +0200

    Mail composer working

--- source/app.d
+++ source/app.d
@@ -6,6 +6,8 @@ import std.json;
 import std.conv : to;
 import std.string : cmp, split;
 import gogga;
+import std.file;
+import std.conv : to;
 
 string VERSION = "vPOES.POES.POES";
 
@@ -109,6 +111,39 @@ void main(string[] args)
 				gprintln("No to was provided");
 				return;
 			}
+
+			/* Read in from the tty */
+			File tty;
+			tty.open("/dev/stdin", "r");
+
+
+			/* Body lines */
+			string bodyLines;
+
+			while(true)
+			{
+				/* Assume each line has a max, only read the max */
+				byte[] bodyLine;
+				bodyLine.length = 30;
+
+				/* Read a line */
+				bodyLine = tty.rawRead(bodyLine);
+				
+				/**
+				* Read returns then we are done reading, either the bodyLine.length
+				* is non-zero but if it is zero then nothing was read (hence an EOF)
+				* signal
+				*/
+				if(bodyLine.length == 0)
+				{
+					break;
+				}	
+
+				/* Store the line */
+				bodyLines ~= bodyLine;
+			}
+		
+			writeln("Mail: "~bodyLines);
 		}
 		/* Unknown command */
 		else
