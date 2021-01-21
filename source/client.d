9c55646ea1c59f97daab443251300d2132ba7890
Tristan B. Kildaire <deavmi@disroot.org>
2021-01-21 12:59:13 +0200

    Fixed bug caused by recursive call that caused the process to open too many files.

--- source/client.d
+++ source/client.d
@@ -5,35 +5,34 @@ import std.stdio;
 string composeMail()
 {
     /* Read in from the tty */
-			File tty;
-			tty.open("/dev/stdin", "r");
+	File tty;
+	tty.open("/dev/stdin", "r");
 
+	/* Body lines */
+	string bodyLines;
 
-			/* Body lines */
-			string bodyLines = composeMail();
+	while(true)
+	{
+		/* Assume each line has a max, only read the max */
+		byte[] bodyLine;
+		bodyLine.length = 30;
 
-			while(true)
-			{
-				/* Assume each line has a max, only read the max */
-				byte[] bodyLine;
-				bodyLine.length = 30;
-
-				/* Read a line */
-				bodyLine = tty.rawRead(bodyLine);
+		/* Read a line */
+		bodyLine = tty.rawRead(bodyLine);
 				
-				/**
-				* Read returns then we are done reading, either the bodyLine.length
-				* is non-zero but if it is zero then nothing was read (hence an EOF)
-				* signal
-				*/
-				if(bodyLine.length == 0)
-				{
-					break;
-				}	
+		/**
+		* Read returns then we are done reading, either the bodyLine.length
+		* is non-zero but if it is zero then nothing was read (hence an EOF)
+		* signal
+		*/
+		if(bodyLine.length == 0)
+		{
+			break;
+		}	
 
-				/* Store the line */
-				bodyLines ~= bodyLine;
-			}
+		/* Store the line */
+		bodyLines ~= bodyLine;
+	}
 
-            return bodyLines;
+    return bodyLines;
 }
